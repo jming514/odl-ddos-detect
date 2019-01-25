9a0821e59d69a9afada7435565fae950d7a7769b
kamitsu <kamitsus@gmail.com>
2019-01-25 01:57:37 -0500

    Update odlreq.py

--- odlreq.py
+++ odlreq.py
@@ -7,7 +7,7 @@ r = requests.get('http://192.168.101.129:8181/restconf/operational/opendaylight-
 #print r.json()
 
 data = r.json()
-with open('flowData.json', 'w') as f:
+with open('flowData3.json', 'w') as f:
     json.dump(data, f)
 
 
