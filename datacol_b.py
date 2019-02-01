b2e181c324856edc17d021f8e4fc05ae4f6d5aab
kamitsu <kamitsus@gmail.com>
2019-02-01 12:04:31 -0500

    fixed indent

--- datacol_b.py
+++ datacol_b.py
@@ -13,25 +13,25 @@ def collectData():
     with open('flowData.json', 'r') as f:
         data = json.load(f)
 
-	numOfCon = len(data['nodes']['node'][1]['node-connector'])
+    numOfCon = len(data['nodes']['node'][1]['node-connector'])
 
-	for i in range(numOfCon):
-		a = data['nodes']['node'][1]['node-connector'][i]['id']
-	
-		if a == 'openflow:2:1':
-			b = data['nodes']['node'][1]['node-connector'][i]['opendaylight-port-statistics:flow-capable-node-connector-statistics']['packets']['received']
-			c = data['nodes']['node'][1]['node-connector'][i]['opendaylight-port-statistics:flow-capable-node-connector-statistics']['packets']['transmitted']
-			d = data['nodes']['node'][1]['node-connector'][i]['opendaylight-port-statistics:flow-capable-node-connector-statistics']['bytes']['received']
-			e = data['nodes']['node'][1]['node-connector'][i]['opendaylight-port-statistics:flow-capable-node-connector-statistics']['bytes']['transmitted']
+    for i in range(numOfCon):
+        a = data['nodes']['node'][1]['node-connector'][i]['id']
 
-			entry = [a, b, c, d, e]
+        if a == 'openflow:2:1':
+            b = data['nodes']['node'][1]['node-connector'][i]['opendaylight-port-statistics:flow-capable-node-connector-statistics']['packets']['received']
+            c = data['nodes']['node'][1]['node-connector'][i]['opendaylight-port-statistics:flow-capable-node-connector-statistics']['packets']['transmitted']
+            d = data['nodes']['node'][1]['node-connector'][i]['opendaylight-port-statistics:flow-capable-node-connector-statistics']['bytes']['received']
+            e = data['nodes']['node'][1]['node-connector'][i]['opendaylight-port-statistics:flow-capable-node-connector-statistics']['bytes']['transmitted']
 
-			with open('flowDataset.txt', 'a') as f:
-				for item in entry:
-				    f.write('%s\t' % item)
-				f.write('\n')
-		else:
-			pass
+            entry = [a, b, c, d, e]
+
+            with open('flowDataset.txt', 'a') as f:
+                for item in entry:
+                    f.write('%s\t' % item)
+                f.write('\n')
+        else:
+            pass
 
 
 if __name__ == '__main__':
