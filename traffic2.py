951939ef9e9b898266f5b736bbbb1773a707040e
kamitsu <kamitsus@gmail.com>
2019-02-07 22:57:51 -0500

    Update

--- traffic2.py
+++ traffic2.py
@@ -14,7 +14,7 @@ def nettraffic():
         port = 12345
 
         # this is the inter-arrival time AND the start time
-        join_time = [0.8, 0.9, 1, 1.1, 1.2]
+        join_time = [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7]
         time.sleep(random.choice(join_time))
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,14 +32,14 @@ def nettraffic():
 
         # number of GET requests per connection
         y = 0
-        reqPerConn = 5
-        while (y < reqPerConn):
+        reqPerConn = [5, 6, 7]
+        while (y < random.choice(reqPerConn)):
             s.sendall(msg)
             reply = s.recv(2048)
             print(reply)
 
             # duration of the connection
-            inter_time = [1.5, 1.6, 1.7]
+            inter_time = [1.3, 1.4, 1.5, 1.6]
             time.sleep(random.choice(inter_time))
             y += 1
         x + 1
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     time.sleep(45)
 
     # each client will have range(x) simultaneous connections to the server
-    for i in range(5):
+    for i in range(10):
         t = Thread(target=nettraffic)
         t.start()
         time.sleep(0.5)
