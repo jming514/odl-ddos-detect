bd2c41ce83b7cc145c11af6669897de18dbcd8e7
kamitsu <33611233+kamitsu@users.noreply.github.com>
2019-03-25 13:13:17 -0400

    Add files via upload

--- livegraph.py
+++ livegraph.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 from matplotlib.figure import Figure
 
+"""
+(1) Negative values only appear when the script traffic script is stopped
+    then run again. The accumulated packets & bytes are reset and cause a 
+    negative difference.
+"""
+
 style.use("ggplot")
 
 fig = plt.figure("Network Traffic")
@@ -10,29 +16,41 @@ ax1 = fig.add_subplot(1, 1, 1)
 
 
 def animate(i):
-    graph_data = open("tester.csv", "r").read()
-    lines = graph_data.split("\n")
-    xs = []
-    ys = []
-    for line in lines:
+    graph_data = open("flowDataset6.csv", "r")
+    # skip first line
+    next(graph_data)
+    readData = graph_data.read()
+    lines = readData.split("\n")
+    xs = []  # Rx pkts
+    ys = []  # Tx pkts
+    ls = []  # Label
+    # append the latest 100 data points
+    for line in lines[-150:]:
         if len(line) > 1:
-            x, y, a, b = line.split(",")
-            xs.append(int(x))
-            ys.append(int(y))
+            x, y, a, b, l = line.split(",")
+            # skip negative values (1)
+            if int(x) < 0:
+                pass
+            else:
+                xs.append(int(x))
+                ys.append(int(y))
+                ls.append(int(l))
 
     xAxis = list(range(0, len(xs)))
     ax1.clear()
     # set up title, axes, legend
-    ax1.set_xticks(xAxis)
     ax1.set_title("Network Traffic")
     ax1.set_xlabel("Data points")
     ax1.set_ylabel("Number of packets")
-    ax1.plot(xAxis, xs, "-v", ys, "-v")
+    ax1.plot(xAxis, xs, ".-", ys, ".-")
     # change the marker based on label
     for i in xAxis:
-        if xs[i] > 200:
-            ax1.plot(xAxis[i], xs[i], "bo-", xAxis[i], ys[i], "ro-")
+        # if "ddos" data point
+        if ls[i] == 1:
+            ax1.plot(xAxis[i], xs[i], "kx-", xAxis[i], ys[i], "kx-")
+
+            # include another title stating 'currently being attacked' or something similar
 
 
-ani = animation.FuncAnimation(fig, animate, interval=2000)
+ani = animation.FuncAnimation(fig, animate, interval=5000)
 plt.show()
--- testgraph.py
+++ testgraph.py
@@ -16,8 +16,9 @@ from tkinter import ttk
 LARGE_FONT = ("Verdana", 12)
 style.use("ggplot")
 
+f = Figure()
 # f = Figure(figsize=(5,5), dpi=100)
-# a = f.add_subplot(111)
+a = f.add_subplot(111)
 
 
 class SeaofBTCapp(tk.Tk):
@@ -52,27 +53,27 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text="Network Traffic", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
         
-        f = Figure(figsize=(5,5), dpi=100)
-        a = f.add_subplot(111)
-        pullData = open("tester.csv","r").read()
-        dataList = pullData.split('\n')
-        xPkt = []
-        yPkt = []
-        xB = []
-        yB = []
-        for eachLine in dataList:
-            if len(eachLine) > 1:
-                x, y, i, j = eachLine.split(',')
-                xPkt.append(int(x))
-                yPkt.append(int(y))
-                xB.append(int(i))
-                yB.append(int(j))
+        # f = Figure(figsize=(5,5), dpi=100)
+        # a = f.add_subplot(111)
+        # pullData = open("tester.csv","r").read()
+        # dataList = pullData.split('\n')
+        # xPkt = []
+        # yPkt = []
+        # xB = []
+        # yB = []
+        # for eachLine in dataList:
+        #     if len(eachLine) > 1:
+        #         x, y, i, j = eachLine.split(',')
+        #         xPkt.append(int(x))
+        #         yPkt.append(int(y))
+        #         xB.append(int(i))
+        #         yB.append(int(j))
                 
-                a.clear()
-                a.plot(xPkt)
-                a.plot(yPkt)
-                a.plot(xB)
-                a.plot(yB)
+        #         a.clear()
+        #         a.plot(xPkt)
+        #         a.plot(yPkt)
+        #         # a.plot(xB)
+        #         # a.plot(yB)
 
         canvas = FigureCanvasTkAgg(f, self)
         canvas.draw()
@@ -82,23 +83,25 @@ class StartPage(tk.Frame):
         toolbar.update()
         canvas._tkcanvas.pack()
         
-        # sself.ani = animation.FuncAnimation(f, self.animate, interval=1000)
+        self.ani = animation.FuncAnimation(f, animate, interval=1000)
         
-    def animate():
-        pullData = open("tester.txt","r").read()
-        dataList = pullData.split('\n')
-        xPkt = []
-        yPkt = []
-        xB = []
-        yB = []
-        for eachLine in dataList:
-            if len(eachLine) > 1:
-                x, y = eachLine.split(',\"')
-                xPkt.append(int(x))
-                yPkt.append(int(y))
-                
-                a.clear()
-                a.plot(xPkt, yPkt)
+
+def animate(i):
+    pullData = open("tester.csv","r").read()
+    dataList = pullData.split('\n')
+    xPkt = []
+    yPkt = []
+    # xB = []
+    # yB = []
+    for eachLine in dataList:
+        if len(eachLine) > 1:
+            x, y, a, b = eachLine.split(',')
+            xPkt.append(int(x))
+            yPkt.append(int(y))
+            
+            a.clear()
+            a.plot(xPkt) 
+            a.plot(yPkt)
 
 app = SeaofBTCapp()
 #ani = animation.FuncAnimation(f, animate, interval=1000)
